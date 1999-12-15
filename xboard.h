72602991c113035df2c74969e74a193abadffc66
gtw <gtw>
1999-12-15 01:17:34 +0000

    Initial revision

---
+++ xboard.h
@@ -0,0 +1,18 @@
+/*
+ * xboard.h
+ *
+ * by Gary Wong, 1997-1999
+ *
+ * $Id: xboard.h,v 1.1 1999/12/15 01:17:34 gtw Exp $
+ */
+
+#ifndef _BOARD_H_
+#define _BOARD_H_
+
+#include <ext.h>
+
+extern extwindowclass ewcBoard;
+
+extern int BoardSet( extwindow *pewnd, char *sz );
+
+#endif
