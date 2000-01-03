10f2e27e13afe21aeee9d04473bc332ef15c1c50
gtw <gtw>
2000-01-03 17:50:40 +0000

    Added rand_r() for systems that don't have it.

---
+++ lib/rand_r.h
@@ -0,0 +1,13 @@
+/*
+ * rand_r.h
+ *
+ * by Gary Wong, 2000
+ *
+ * $Id: rand_r.h,v 1.1 2000/01/03 17:50:40 gtw Exp $
+ */
+
+#ifndef _RAND_R_H_
+
+extern unsigned int rand_r( unsigned int *pnSeed );
+
+#endif
