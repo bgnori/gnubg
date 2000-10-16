ac44d283f58365ceac6e3556cd41a3a203cdcf86
gtw <gtw>
2000-10-16 15:38:44 +0000

    Add `-w' (--window-system-only) option to disable TTY input/output.

--- gtkgame.h
+++ gtkgame.h
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: gtkgame.h,v 1.6 2000/08/02 19:21:43 gtw Exp $
+ * $Id: gtkgame.h,v 1.7 2000/10/16 15:38:44 gtw Exp $
  */
 
 #ifndef _GTKGAME_H_
@@ -36,6 +36,6 @@ extern int GTKRolloutUpdate( float arMu[], float arSigma[], int i, int c );
 extern void GTKRolloutDone( void );
 extern void GTKSet( void *p );
 
-extern int fGTKOutput;
+extern int fGTKOutput, fTTY;
 
 #endif
