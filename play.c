1e93c7b54a32f98ee1218b46d349e6921b92d8dd
gtw <gtw>
2000-11-29 16:36:49 +0000

    Crawford rule fixes.

--- play.c
+++ play.c
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: play.c,v 1.52 2000/11/14 15:26:38 gtw Exp $
+ * $Id: play.c,v 1.53 2000/11/29 16:36:49 gtw Exp $
  */
 
 #include "config.h"
@@ -115,7 +115,9 @@ static void ApplyMoveRecord( moverecord *pmr ) {
 	anDice[ 0 ] = anDice[ 1 ] = 0;
 	fResigned = fDoubled = FALSE;
 	nCube = 1 << pmr->g.nAutoDoubles;
-    
+	fCrawford = pmr->g.fCrawfordGame;
+	fPostCrawford = !fCrawford && ( anScore[ 0 ] == nMatchTo - 1 ||
+					anScore[ 1 ] == nMatchTo - 1 );
 	break;
 	
     case MOVE_DOUBLE:
@@ -807,10 +809,10 @@ extern void NextTurn( void ) {
 #endif
 	
 	if( nMatchTo && fAutoCrawford ) {
-	    fPostCrawford = fCrawford && anScore[ pmgi->fWinner ] < nMatchTo;
+	    fPostCrawford |= fCrawford && anScore[ pmgi->fWinner ] < nMatchTo;
 	    fCrawford = !fPostCrawford && !fCrawford &&
 		anScore[ pmgi->fWinner ] == nMatchTo - 1 &&
-		anScore[ !pmgi->fWinner ] == nMatchTo - 1;
+		anScore[ !pmgi->fWinner ] != nMatchTo - 1;
 	}
 	
 	CommandShowScore( NULL );
