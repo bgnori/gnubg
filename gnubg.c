3d24b90977432cb38934fb5ac326f1b17372ff1e
gtw <gtw>
2001-02-07 17:59:16 +0000

    Don't restart system calls on SIGINT.

--- gnubg.c
+++ gnubg.c
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: gnubg.c,v 1.93 2001/01/31 15:39:20 gtw Exp $
+ * $Id: gnubg.c,v 1.94 2001/02/07 17:59:16 gtw Exp $
  */
 
 #include "config.h"
@@ -749,7 +749,7 @@ static void PortableSignal( int nSignal, RETSIGTYPE (*p)(int),
     sigemptyset( &sa.sa_mask );
     sa.sa_flags =
 # if SA_RESTART
-	SA_RESTART |
+	nSignal == SIGINT ? 0 : SA_RESTART |
 # endif
 # if SA_NOCLDSTOP
 	SA_NOCLDSTOP |
@@ -761,7 +761,7 @@ static void PortableSignal( int nSignal, RETSIGTYPE (*p)(int),
 
     sv.sv_handler = p;
     sigemptyset( &sv.sv_mask );
-    sv.sv_flags = 0;
+    sv.sv_flags = nSignal == SIGINT ? SV_INTERRUPT : 0;
 
     sigvec( nSignal, &sv, pOld );
 #else
@@ -2804,7 +2804,8 @@ static void real_main( void *closure, int argc, char *argv[] ) {
 #if USE_GTK
     if( fTTY )
 #endif
-	puts( "GNU Backgammon " VERSION "  Copyright 1999, 2000 Gary Wong.\n"
+	puts( "GNU Backgammon " VERSION "  Copyright 1999, 2000, 2001 "
+	      "Gary Wong.\n"
 	      "GNU Backgammon is free software, covered by the GNU "
 	      "General Public License\n"
 	      "version 2, and you are welcome to change it and/or "
