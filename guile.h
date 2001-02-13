d3debdbfecd90b524f0239d64a9e372c9e9e7c59
gtw <gtw>
2001-02-13 18:21:20 +0000

    Signal handling improvements.

--- guile.h
+++ guile.h
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: guile.h,v 1.2 2000/11/13 15:55:49 gtw Exp $
+ * $Id: guile.h,v 1.3 2001/02/13 18:21:20 gtw Exp $
  */
 
 #if USE_GUILE
@@ -27,6 +27,8 @@
 #define GNUBG_SCM "gnubg.scm"
 
 extern int GuileInitialise( char *szDir );
+extern int GuileStartIntHandler( void );
+extern int GuileEndIntHandler( void );
 
 #endif
 
