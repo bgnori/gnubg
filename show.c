ecc7d5af4e7c1e756497620bebd91f89c5a3d7e8
thyssen <thyssen>
2001-05-23 15:12:15 +0000

    evaltype moved into evalsetup.

--- show.c
+++ show.c
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: show.c,v 1.64 2001/05/21 14:46:04 gtw Exp $
+ * $Id: show.c,v 1.65 2001/05/23 15:12:29 thyssen Exp $
  */
 
 #include "config.h"
@@ -110,9 +110,9 @@ ShowRollout ( rolloutcontext *prc ) {
 
 
 extern void
-ShowEvalSetup ( evaltype *pet, evalsetup *pes ) {
+ShowEvalSetup ( evalsetup *pes ) {
 
-  switch ( *pet ) {
+  switch ( pes->et ) {
 
   case EVAL_NONE:
     outputl ( "      No evaluation." );
@@ -213,9 +213,9 @@ extern void CommandShowAnalysis( char *sz ) {
              "The analysis will be perform with the "
              "following evaluation paremters:" );
     outputl( "    Chequer play:" );
-    ShowEvalSetup ( &etAnalysisChequer, &esAnalysisChequer );
+    ShowEvalSetup ( &esAnalysisChequer );
     outputl( "    Cube decisions:" );
-    ShowEvalSetup ( &etAnalysisCube, &esAnalysisCube );
+    ShowEvalSetup ( &esAnalysisCube );
 
     
 
@@ -422,9 +422,9 @@ extern void CommandShowEvaluation( char *sz ) {
 
     outputl( "`eval' and `hint' will use:" );
     outputl( "    Chequer play:" );
-    ShowEvalSetup ( &etEvalChequer, &esEvalChequer );
+    ShowEvalSetup ( &esEvalChequer );
     outputl( "    Cube decisions:" );
-    ShowEvalSetup ( &etEvalCube, &esEvalCube );
+    ShowEvalSetup ( &esEvalCube );
 
 }
 
@@ -481,9 +481,9 @@ extern void CommandShowPlayer( char *sz ) {
 	case PLAYER_GNU:
 	    outputf( "gnubg:\n" );
             outputl( "    Checker play:" );
-            ShowEvalSetup ( &ap[ i ].etChequer, &ap[ i ].esChequer );
+            ShowEvalSetup ( &ap[ i ].esChequer );
             outputl( "    Cube decisions:" );
-            ShowEvalSetup ( &ap[ i ].etCube, &ap[ i ].esCube );
+            ShowEvalSetup ( &ap[ i ].esCube );
 	    break;
 	case PLAYER_PUBEVAL:
 	    outputl( "pubeval\n" );
