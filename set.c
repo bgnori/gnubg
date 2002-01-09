11c6904c6f2a3cc56e9d8e309ade7ef246e66e73
gtw <gtw>
2002-01-09 17:52:16 +0000

    #include <limits.h> wherever it is available, not just for Suns.

--- set.c
+++ set.c
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: set.c,v 1.84 2001/12/26 22:46:15 joseph Exp $
+ * $Id: set.c,v 1.85 2002/01/09 17:52:16 gtw Exp $
  */
 
 #include "config.h"
@@ -24,16 +24,12 @@
 #include <assert.h>
 #include <ctype.h>
 #include <errno.h>
+#if HAVE_LIMITS_H
+#include <limits.h>
+#endif
 #include <math.h>
 #if HAVE_SYS_SOCKET_H
 #include <sys/types.h>
-
-#if defined(sun)
-#define AF_LOCAL AF_UNIX
-#define PF_LOCAL PF_UNIX
-#include <limits.h>
-#endif
-
 #include <sys/socket.h>
 #include <sys/un.h>
 #endif
