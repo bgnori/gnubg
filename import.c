cd7706e9fe6d64f61580e75b0352b7ce32a3aadb
thyssen <thyssen>
2002-07-15 21:09:15 +0000

    add RCS keyword

--- import.c
+++ import.c
@@ -16,6 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
+ * $Id: import.c,v 1.31 2002/07/15 21:09:15 thyssen Exp $
  */
 
 #include "config.h"
