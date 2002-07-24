6fcc2d5a9f29fd247df267e93b1611b816219e17
thyssen <thyssen>
2002-07-24 16:49:14 +0000

    Tutor mode (contributed by Jim Segrave).

--- gtkgame.h
+++ gtkgame.h
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: gtkgame.h,v 1.45 2002/07/17 18:46:09 gtw Exp $
+ * $Id: gtkgame.h,v 1.46 2002/07/24 16:50:09 thyssen Exp $
  */
 
 #ifndef _GTKGAME_H_
@@ -127,5 +127,9 @@ extern void SetRollouts( gpointer *p, guint n, GtkWidget *pw );
 extern void HintMove( GtkWidget *pw, GtkWidget *pwMoves );
 
 extern int fTTY;
+
+extern int 
+GtkTutor ( char *sz );
+
 
 #endif
