92c97c406d71ba02fe94c689720f1a924f87aad6
gtw <gtw>
2002-09-14 16:07:56 +0000

    Temporarily comment out broken code.

--- sound.c
+++ sound.c
@@ -20,7 +20,7 @@
  * File modified by Joern Thyssen <jthyssen@dk.ibm.com> for use with
  * GNU Backgammon.
  *
- * $Id: sound.c,v 1.11 2002/09/14 15:33:40 gtw Exp $
+ * $Id: sound.c,v 1.12 2002/09/14 16:07:56 gtw Exp $
  */
 
 #ifdef HAVE_CONFIG_H
@@ -971,10 +971,13 @@ playSound ( const gnubgsound gs ) {
 
 extern void SoundFlushCache( const gnubgsound gs ) {
 
+#if 0
+    /* FIXME This code breaks sound playback -- why? */
 #ifdef SIGIO
     if( ssSoundSystem == SOUND_SYSTEM_NORMAL )
 	/* the sound might be in use at the moment */
 	SoundWait();
+#endif
 #endif
     
     if( asc[ gs ].pch ) {
