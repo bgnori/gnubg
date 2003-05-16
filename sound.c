ee2ed87269138d3e10ab67902b8940b0cab98ce1
thyssen <thyssen>
2003-05-16 08:08:34 +0000

    Fix problems with _af_ulaw2linear in Redhat 9 (reported by Rod Roark)

--- sound.c
+++ sound.c
@@ -20,7 +20,7 @@
  * File modified by Joern Thyssen <jthyssen@dk.ibm.com> for use with
  * GNU Backgammon.
  *
- * $Id: sound.c,v 1.19 2003/04/05 18:42:59 thyssen Exp $
+ * $Id: sound.c,v 1.20 2003/05/16 08:09:09 thyssen Exp $
  */
 
 #ifdef HAVE_CONFIG_H
@@ -628,6 +628,46 @@ static char *can_play_audio( void ) {
 #endif  /* #ifndef WIN32 */
 
 #ifdef HAVE_ARTSC
+
+
+static int _af_ulaw2linear(unsigned char ulawbyte);
+
+#ifndef HAVE_AUDIOFILE
+
+/*
+** This routine converts from ulaw to 16 bit linear.
+**
+** Craig Reese: IDA/Supercomputing Research Center
+** 29 September 1989
+**
+** References:
+** 1) CCITT Recommendation G.711  (very difficult to follow)
+** 2) MIL-STD-188-113,"Interoperability and Performance Standards
+**     for Analog-to_Digital Conversion Techniques,"
+**     17 February 1987
+**
+** Input: 8 bit ulaw sample
+** Output: signed 16 bit linear sample
+** Z-note -- this is from libaudiofile.  Thanks guys!
+*/
+
+static int _af_ulaw2linear(unsigned char ulawbyte)
+{
+	static int exp_lut[8] = { 0, 132, 396, 924, 1980, 4092, 8316, 16764 };
+	int sign, exponent, mantissa, sample;
+
+	ulawbyte = ~ulawbyte;
+	sign = (ulawbyte & 0x80);
+	exponent = (ulawbyte >> 4) & 0x07;
+	mantissa = ulawbyte & 0x0F;
+	sample = exp_lut[exponent] + (mantissa << (exponent + 3));
+	if (sign != 0)
+		sample = -sample;
+
+	return (sample);
+}
+
+#endif /* ! HAVE_AUDIOFILE */
 
 static int play_artsc(unsigned char *data, int size)
 {
