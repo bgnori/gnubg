1ede7bb809a42aea6cecd90a1f406b74c6049e2a
Superfly_Jon <Superfly_Jon>
2003-09-12 08:54:56 +0000

    typo

--- html.c
+++ html.c
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: html.c,v 1.145 2003/09/06 12:26:11 thyssen Exp $
+ * $Id: html.c,v 1.146 2003/09/12 08:54:56 Superfly_Jon Exp $
  */
 
 #include "config.h"
@@ -171,7 +171,7 @@ WriteStyleSheet ( FILE *pf, const htmlexportcss hecss ) {
 
     fputs( "\n"
            "/* CSS Stylesheet for GNU Backgammon " VERSION " */\n"
-           "/* $Id: html.c,v 1.145 2003/09/06 12:26:11 thyssen Exp $ */\n",
+           "/* $Id: html.c,v 1.146 2003/09/12 08:54:56 Superfly_Jon Exp $ */\n",
            pf );
 
     fputs( _("/* This file is distributed as a part of the "
@@ -1810,7 +1810,7 @@ HTMLEpilogue ( FILE *pf, const matchstate *pms, char *aszLinks[ 4 ],
   int fFirst;
   int i;
 
-  const char szVersion[] = "$Revision: 1.145 $";
+  const char szVersion[] = "$Revision: 1.146 $";
   int iMajor, iMinor;
 
   iMajor = atoi ( strchr ( szVersion, ' ' ) );
@@ -1891,7 +1891,7 @@ HTMLEpilogueComment ( FILE *pf ) {
 
   time_t t;
 
-  const char szVersion[] = "$Revision: 1.145 $";
+  const char szVersion[] = "$Revision: 1.146 $";
   int iMajor, iMinor;
   char *pc;
 
@@ -2577,7 +2577,7 @@ HTMLPrintMoveAnalysis ( FILE *pf, matchstate *pms, moverecord *pmr,
 
         float *ar = pmr->n.ml.amMoves[ i ].arEvalMove;
 
-        /*,A (Bpercentages */
+        /* percentages */
 
         if ( i == pmr->n.iMove )
           fprintf ( pf, "<tr %s>\n", 
