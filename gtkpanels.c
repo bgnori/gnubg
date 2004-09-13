ee467be2bbcdeb926478c073c9f8fd352fb663c5
Superfly_Jon <Superfly_Jon>
2004-09-13 14:30:18 +0000

    gtk2 build problem

--- gtkpanels.c
+++ gtkpanels.c
@@ -18,10 +18,11 @@
 * along with this program; if not, write to the Free Software
 * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 *
-* $Id: gtkpanels.c,v 1.2 2004/09/13 12:43:12 Superfly_Jon Exp $
+* $Id: gtkpanels.c,v 1.3 2004/09/13 14:30:18 Superfly_Jon Exp $
 */
 
 #include "config.h"
+#define GTK_ENABLE_BROKEN /* for GtkText */
 #include "backgammon.h"
 #include "gtkboard.h"
 #include <i18n.h>
