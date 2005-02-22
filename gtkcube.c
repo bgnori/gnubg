80082c018bd083719d86594c2d0c440812702334
oysteijo <oysteijo>
2005-02-22 18:27:14 +0000

    Get the evalcontext right in gtkcube.c

--- gtkcube.c
+++ gtkcube.c
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: gtkcube.c,v 1.40 2004/11/24 10:27:00 Superfly_Jon Exp $
+ * $Id: gtkcube.c,v 1.41 2005/02/22 18:27:15 oysteijo Exp $
  */
 
 #if HAVE_CONFIG_H
@@ -783,14 +783,14 @@ CubeAnalysisEvalPly ( GtkWidget *pw, cubehintdata *pchd ) {
 
   char *szPly = gtk_object_get_data ( GTK_OBJECT ( pw ), "user_data" );
   //evalcontext ec = { TRUE, 0, 0, TRUE, 0.0 };
-  evalcontext ec = {  esAnalysisCube.ec.fCubeful,
-		      0, TRUE, 0.0, 
+  evalcontext ec = {  esAnalysisCube.ec.fCubeful, 0,
 #if defined( REDUCTION_CODE )
-  esAnalysisCube.ec.nReduced
+                      esAnalysisCube.ec.nReduced,
 #else
-  esAnalysisCube.ec.fUsePrune
+                      esAnalysisCube.ec.fUsePrune,
 #endif
-   };
+                      TRUE, 0.0};
+
   ec.nPlies = atoi ( szPly );
 
   EvalCube ( pchd, &ec );
