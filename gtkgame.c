068fa8c4f5c92f292791f4d080bcd1750648a0ae
c_anthon <c_anthon>
2006-10-16 13:59:46 +0000

    fixes to language settings

--- gtkgame.c
+++ gtkgame.c
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: gtkgame.c,v 1.593 2006/10/13 16:50:56 Superfly_Jon Exp $
+ * $Id: gtkgame.c,v 1.594 2006/10/16 13:59:46 c_anthon Exp $
  */
 
 #if HAVE_CONFIG_H
@@ -9676,15 +9676,15 @@ GtkWidget *pwLangDialog, *pwLangRadio1, *pwLangRadio2, *pwLangTable;
 static char *aaszLang[][ 3 ] = {
     { N_("System default"), "system", NULL },
     { N_("Czech"),	    "cs_CZ", "czech" },
-    { N_("Danish"),	    "da_DA", "denmark" },
-    { N_("English (UK)"),   "en_UK", "england" },
+    { N_("Danish"),	    "da_DK", "denmark" },
+    { N_("English (GB)"),   "en_GB", "england" },
     { N_("English (US)"),   "en_US", "usa" },
     { N_("French"),	    "fr_FR", "france" },
     { N_("German"),	    "de_DE", "germany" },
     { N_("Icelandic"),      "is_IS", "iceland" },
     { N_("Italian"),	    "it_IT", "italy" },
     { N_("Japanese"),	    "ja_JP", "japan" },
-	{ N_("Russian"),	    "ru_RU", "russia" },
+    { N_("Russian"),	    "ru_RU", "russia" },
     { N_("Turkish"),	    "tr_TR", "turkey" },
     { NULL, NULL, NULL }
 };
@@ -9912,8 +9912,7 @@ static void SetLanguage( gpointer *p, guint n, GtkWidget *pw )
 
 	if (newLang)
 	{	/* Set new language (after dialog has closed) */
-		char sz[100];
-		sprintf(sz, "set lang %s", newLang);
-		UserCommand( sz );
+                SetupLanguage(newLang);
+		GtkChangeLanguage();
 	}
 }
