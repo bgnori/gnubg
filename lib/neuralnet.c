8417bc044ba2b65e216a2409080db9b2864e7ab7
c_anthon <c_anthon>
2007-02-08 11:33:31 +0000

    A few minor thread changes

--- lib/neuralnet.c
+++ lib/neuralnet.c
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: neuralnet.c,v 1.44 2007/02/02 19:07:43 Superfly_Jon Exp $
+ * $Id: neuralnet.c,v 1.45 2007/02/08 11:33:31 c_anthon Exp $
  */
 
 #include "config.h"
@@ -439,9 +439,6 @@ extern void *NeuralNetCreateDirect( neuralnet *pnn, void *p ) {
    fp += pnn->cHidden;
    pnn->arOutputThreshold = fp;
    fp += pnn->cOutput;
-
-   pnn->savedBase = malloc( pnn->cHidden * sizeof( float ) ); 
-   pnn->savedIBase = malloc( pnn->cInput * sizeof( float ) ); 
 
    return fp;
 }
