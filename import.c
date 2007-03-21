008199295c28f7bf3fe8db78f0547a6447760be8
c_anthon <c_anthon>
2007-03-21 22:31:01 +0000

    protect strptime, which isn't around on windows

--- import.c
+++ import.c
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: import.c,v 1.114 2007/03/20 23:22:45 c_anthon Exp $
+ * $Id: import.c,v 1.115 2007/03/21 22:31:01 c_anthon Exp $
  */
 
 #include "config.h"
@@ -105,6 +105,7 @@ ParseSetDate ( char *szFilename ) {
     struct tm   mdate;
 
     matchdate = NULL;
+#if HAVE_STRPTIME
     for ( pch = szFilename; *pch != '\0'; pch++ ); /* goto end of filename */
     while ( pch != szFilename ) {
         if ( *--pch == '-' ) {                     /* go backwards until '-' */
@@ -115,6 +116,7 @@ ParseSetDate ( char *szFilename ) {
             }
         }
     }
+#endif
     /* date could not be parsed out of filename, use last access date */
     if ( matchdate == NULL ) {
         if ( stat( szFilename, &filestat ) == 0 ) {
