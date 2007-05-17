343bd5025438b5a5ce6102baf4a73437b0303758
c_anthon <c_anthon>
2007-05-17 19:31:21 +0000

    re-add DIALOG_FLAG_MINMAXBUTTONS. Helps a bit on windows and doesn't make things a lot worse with a more sensible wm.

--- progress.c
+++ progress.c
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: progress.c,v 1.33 2007/05/17 13:57:02 c_anthon Exp $
+ * $Id: progress.c,v 1.34 2007/05/17 19:31:21 c_anthon Exp $
  */
 
 #include "config.h"
@@ -806,7 +806,7 @@ GTKRolloutProgressStart( const cubeinfo *pci, const int n,
   AllocTextList(prp);
 
   prp->pwRolloutDialog = GTKCreateDialog( _("GNU Backgammon - Rollout"), DT_INFO, NULL,
-	  DIALOG_FLAG_MODAL | DIALOG_FLAG_NOTIDY, NULL, NULL );
+	  DIALOG_FLAG_MODAL | DIALOG_FLAG_MINMAXBUTTONS | DIALOG_FLAG_NOTIDY, NULL, NULL );
   prp->pwRolloutViewStat = gtk_button_new_with_label ( _("View statistics") );
   prp->pwRolloutStop = gtk_button_new_with_label( _("Stop") );
     
