f2cf7c5c097a4f30598e28a165b1100857700951
ace <ace>
2007-07-02 15:56:29 +0000

    *** empty log message ***

--- credits.sh
+++ credits.sh
@@ -17,7 +17,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #
-# $Id: credits.sh,v 1.125 2007/07/02 15:59:53 ace Exp $
+# $Id: credits.sh,v 1.124 2007/07/02 15:56:29 ace Exp $
 # 
 
 column < /dev/null || exit 0
@@ -234,20 +234,7 @@ EOF
 
 cat > credits.c <<EOF
 /* Do not modify this file!  It is created automatically by credits.sh.
-   Modify credits.sh instead. 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of version 3 or later of the GNU General Public License as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*/
+   Modify credits.sh instead. */
 
 #include "config.h"
 #include <glib/gi18n.h>
