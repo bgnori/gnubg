252a6f2a25591d07e7af6b6f8f77482103800bf2
c_anthon <c_anthon>
2007-12-19 10:07:12 +0000

    only lock one level

--- lib/cache.c
+++ lib/cache.c
@@ -15,7 +15,7 @@
  * cache.c
  *
  * by Gary Wong, 1997-2000
- * $Id: cache.c,v 1.15 2007/12/19 09:34:24 c_anthon Exp $
+ * $Id: cache.c,v 1.16 2007/12/19 10:07:12 c_anthon Exp $
  */
 
 #include "config.h"
@@ -37,14 +37,11 @@
 static void cache_unlock(volatile evalCache* pc, volatile unsigned long lock)
 {
 	(void)MT_SafeDec(&pc->locks[lock]);
-	(void)MT_SafeDec(&pc->locks[lock+1]);
 }
 
 static int cache_addlock(volatile evalCache* pc, volatile unsigned long lock)
 {
-	int r1 = MT_SafeInc(&pc->locks[lock]);
-	int r2 = MT_SafeInc(&pc->locks[lock+1]);
-	return (r1 > 1 || r2 > 1);
+	return (MT_SafeInc(&pc->locks[lock])> 1);
 }
 static int cache_lock(volatile evalCache* pc, volatile unsigned long lock)
 {
