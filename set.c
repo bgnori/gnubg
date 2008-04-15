8af476811652c0931c434f437c1bf275e878cefd
Superfly_Jon <Superfly_Jon>
2008-04-15 21:00:20 +0000

    3d cube drawing fix

--- set.c
+++ set.c
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: set.c,v 1.298 2008/03/17 21:39:58 Superfly_Jon Exp $
+ * $Id: set.c,v 1.299 2008/04/15 21:00:20 Superfly_Jon Exp $
  */
 
 #include "config.h"
@@ -725,7 +725,11 @@ extern void CommandSetClockwise( char *sz ) {
                  "player 0 moves clockwise).") );
     
 #if USE_BOARD3D
+{
+	BoardData *bd = BOARD(pwBoard)->board_data;
 	RestrictiveRedraw();
+	RerenderBase(bd->bd3d);
+}
 #endif
 #if USE_GTK
     if( fX )
