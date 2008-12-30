830e9afa1df12eda4c1ce34de5c49872bf4a54a2
Superfly_Jon <Superfly_Jon>
2008-12-30 21:24:47 +0000

    Put temporary hack in win32 only block

--- util.c
+++ util.c
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: util.c,v 1.24 2008/12/29 23:55:29 Superfly_Jon Exp $
+ * $Id: util.c,v 1.25 2008/12/30 21:24:47 Superfly_Jon Exp $
  */
 
 #include "config.h"
@@ -91,6 +91,9 @@ FILE *fdopen(int, const char *);
 
 /* Temporary version of g_file_open_tmp() as glib version doesn't work
    although this code is copied from glib... */
+#ifndef WIN32
+#define TEMP_g_file_open_tmp g_file_open_tmp
+#else
 #include <fcntl.h>
 #include <errno.h>
 
@@ -157,6 +160,7 @@ int TEMP_g_file_open_tmp (const char *tmpl, char      **name_used, GError    **e
   return TEMP_g_mkstemp(*name_used);
 }
 /* End of temporary copy of glib code, remove when glib version works on windows... */
+#endif
 
 extern FILE *GetTemporaryFile(const char *nameTemplate, char **retName)
 {
