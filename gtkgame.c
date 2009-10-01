4c7fecf8bf324daed1a3ee1e415fb6302b25a5a5
c_anthon <c_anthon>
2009-10-01 13:22:37 +0000

    fix a strange looking line

--- gtkgame.c
+++ gtkgame.c
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: gtkgame.c,v 1.794 2009/09/29 22:09:04 c_anthon Exp $
+ * $Id: gtkgame.c,v 1.795 2009/10/01 13:22:37 c_anthon Exp $
  */
 
 #include "config.h"
@@ -2053,7 +2053,7 @@ static int EvalDefaultSetting(evalcontext *pec, movefilter *pmf)
 static void UpdateSummaryEvalMenuSetting(AnalysisDetails *pAnalDetails )
 {
 	int chequerDefault = EvalDefaultSetting(pAnalDetails->esChequer, pAnalDetails->mfChequer);
-	int cubeDefault = EvalDefaultSetting(pAnalDetails->esCube, pAnalDetails->mfCube ? (movefilter*)&pAnalDetails->mfCube : NULL);
+	int cubeDefault = EvalDefaultSetting(pAnalDetails->esCube, pAnalDetails->mfCube);
 	int setting = NUM_SETTINGS;
 	if (chequerDefault == cubeDefault
 		/* Special case as cube_supremo==cube_worldclass */
