fe6e16702645872fe70bd9e9c083861f8e213eae
plm <plm>
2010-09-27 06:30:24 +0000

    XGIDs with large cubes were not correctly decoded

--- set.c
+++ set.c
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: set.c,v 1.343 2010/07/28 14:46:07 c_anthon Exp $
+ * $Id: set.c,v 1.344 2010/09/27 06:30:24 plm Exp $
  */
 
 #include "config.h"
@@ -4255,7 +4255,7 @@ static int SetXGID(char *sz)
 		return 1;
 	}
 
-	nCube = atoi(v[8]) + 1;
+	nCube = 1 << atoi(v[8]);
 
 	switch (atoi(v[7])) {
 	case 1:
