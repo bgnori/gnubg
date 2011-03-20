7952ec93c9ee3bc4b7e00de7d6468df40115c422
mdpetch <mdpetch>
2011-03-20 21:13:42 +0000

    Cleaned up strings for translation

--- gnubgmodule.c
+++ gnubgmodule.c
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: gnubgmodule.c,v 1.106 2011/03/19 19:41:27 mdpetch Exp $
+ * $Id: gnubgmodule.c,v 1.107 2011/03/20 21:16:50 mdpetch Exp $
  */
 
 #include "config.h"
@@ -483,7 +483,7 @@ PythonCubeInfo(PyObject* self UNUSED_PARAM, PyObject* args) {
 
   if ( SetCubeInfo( &ci, nCube, fCubeOwner, fMove, nMatchTo, anScore,
                     fCrawford, fJacoby, fBeavers, bgv ) ) {
-    printf( "error in SetCubeInfo\n" );
+    printf( N_("error in SetCubeInfo\n") );
     return NULL;
   }
 
@@ -509,7 +509,7 @@ PythonPosInfo(PyObject* self UNUSED_PARAM, PyObject* args) {
     return NULL;
 
   if ( SetPosInfo( &pi, fTurn, fResigned, fDoubled, gs, anDice ) ) {
-    printf( "error in SetPosInfo\n" );
+    printf( N_("error in SetPosInfo\n") );
     return NULL;
   }
 
@@ -745,7 +745,7 @@ PythonFindBestMove( PyObject* self UNUSED_PARAM, PyObject *args ) {
 
   if (anDice[0] == 0)
   {
-	  printf("What? No dice?\n");
+	  printf(N_("What? No dice?\n"));
 	  return NULL;
   }
 
