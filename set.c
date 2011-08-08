a704085dcc72f4fcc7957ba6cf75f2664da2a4a1
mdpetch <mdpetch>
2011-08-08 00:17:48 +0000

    Fixed bug where some imported XGID cube positions were not properly swapped when displayed

--- set.c
+++ set.c
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  *
- * $Id: set.c,v 1.352 2011/08/03 23:01:52 mdpetch Exp $
+ * $Id: set.c,v 1.353 2011/08/08 00:17:48 mdpetch Exp $
  */
 
 #include "config.h"
@@ -4184,6 +4184,7 @@ static int SetXGID(char *sz)
 	char *c;
 	int i;
 	char v[9][5];
+	int fSidesSwapped = FALSE;
 
 	for (i = 0; i < 9 && (c = strrchr(s, ':')); i++) {
 		strncpy(v[i], c + 1, 4);
@@ -4342,15 +4343,18 @@ static int SetXGID(char *sz)
 	CommandSetMatchID(matchid);
 	g_free(matchid);
 
-	if (!fMove)
+	if (!fMove){
 		SwapSides(anBoard);
+		fSidesSwapped = TRUE;
+	}
 	posid = g_strdup(PositionID((ConstTanBoard)anBoard));
 	CommandSetBoard(posid);
 	g_free(posid);
 
-	if (anDice[0] && !fMove)
+	if (( anDice[0] == 0 && fSidesSwapped && fCubeOwner != -1 ) ||
+		( anDice[0] && !fMove ) ) {
 		CommandSwapPlayers(NULL);
-
+	}
 	return 0;
 }
 
