df07e9b5dca66976cf705c8cb79844832ceb56c8
plm <plm>
2012-04-23 19:36:31 +0000

    Add missing $Id$ line

--- simpleboard.c
+++ simpleboard.c
@@ -10,6 +10,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * $Id: simpleboard.c,v 1.16 2012/04/23 19:36:31 plm Exp $
  */
 
 /*! \file simpleboard.c
